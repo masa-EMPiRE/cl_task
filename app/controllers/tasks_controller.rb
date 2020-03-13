754f25be00202d07a983c704f487abf30daaf47a
谷崎公紀 <marutto8@gmail.com>
2020-03-13 13:52:50 +0900

    add 一覧、詳細

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,11 @@
+class TasksController < ApplicationController
+
+  def show 
+    @task = Task.find(params[:id])
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Task.create(
+  title: "みきなえんぱいあ",
+  body: "えんぱいあー"
+)
+Task.create(
+  title: "まほえんぱいあ",
+  body: "最後、自由に踊ってください、have it my way!!"
+)
+Task.create(
+  title: "ユメカ・ナウカナ？",
+  body: "きゃりるーです"
+)
+Task.create(
+  title: "月ノウサギ",
+  body: "エグい"
+)
