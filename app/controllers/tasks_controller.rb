cf1581b22a7a0addae326b58a258a5933a477151
谷崎公紀 <marutto8@gmail.com>
2020-03-13 21:48:04 +0900

    indexから削除するとルーティングエラーが起こります

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,9 +15,26 @@ class TasksController < ApplicationController
     redirect_to "/tasks"
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to @tasks
+  end
+
   private
+
   def task_params
-    params.require(:task).permit(:title, :body)
+    params.require.(:task).permit(:title, :body)
   end
 
 end
