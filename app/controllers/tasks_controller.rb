efca22c745210fe2e8b2a909d56ed1b5e102fd7d
谷崎公紀 <marutto8@gmail.com>
2020-03-13 14:35:14 +0900

    add データ登録

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,19 @@ class TasksController < ApplicationController
   end
 
   def index
+    @task = Task.new
     @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to "/tasks"
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :body)
   end
 
 end
